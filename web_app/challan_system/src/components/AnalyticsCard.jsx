28ba99da54191f4434acc8dc4efbc371135a5657
unknown <sahilhsgupta7@gmail.com>
2024-12-03 23:49:38 +0530

    feat: Added analytics card

---
+++ web_app/challan_system/src/components/AnalyticsCard.jsx
@@ -0,0 +1,35 @@
+import { Card, CardBody } from "@nextui-org/react";
+import React from "react";
+
+export default function AnalyticsCard({totalChallan,totalAmount,totalAmountCollected,successfulPayments,successfulDelivery}) {
+  return (
+    <div className="px-8 pt-8">
+      <Card>
+        <CardBody>
+          <div className="flex justify-between items-center px-4 py-4">
+            <div>
+                <p className="text-xs capitalize text-default-400 font-bold">Total Challan</p>
+                <p className="capitalize font-bold text-custom-blue text-2xl">{totalChallan}</p>
+            </div>
+            <div>
+                <p className="text-xs capitalize text-default-400 font-bold">Total Amount</p>
+                <p className="capitalize font-bold text-2xl">₹ {totalAmount}</p>
+            </div>
+            <div>
+                <p className="text-xs capitalize text-default-400 font-bold">Total Amount Collected</p>
+                <p className="capitalize font-bold text-2xl">₹ {totalAmountCollected}</p>
+            </div>
+            <div>
+                <p className="text-xs capitalize text-default-400 font-bold">Successful Payments</p>
+                <p className="capitalize font-bold text-2xl">{successfulPayments}</p>
+            </div>
+            <div>
+                <p className="text-xs capitalize text-default-400 font-bold">Successful Delivery</p>
+                <p className="capitalize font-bold text-2xl">{successfulDelivery}</p>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
--- web_app/challan_system/src/pages/Dashboard.jsx
+++ web_app/challan_system/src/pages/Dashboard.jsx
@@ -12,11 +12,12 @@ import {
   where,
 } from "firebase/firestore";
 import "../firebaseConfig";
+import AnalyticsCard from "../components/AnalyticsCard";
 
 export default function Dashboard() {
   const db = getFirestore();
-  const [challanSearch,setChallanSearch] = useState("");
-  const [isLoading,setIsLoading] = useState(true);
+  const [challanSearch, setChallanSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
   const currentDate = new Date();
   const isFirstOfMonth = currentDate.getDate() === 1;
   const localDate = new Date(
@@ -29,6 +30,11 @@ export default function Dashboard() {
   );
 
   const [challanList, setChallanList] = useState([]);
+  const [totalChallan, setTotalChallan] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [totalAmountCollected, setTotalAmountCollected] = useState(0);
+  const [successfulPayments, setSuccessfulPayments] = useState(0);
+  const [successfulDelivery, setSuccessfulDelivery] = useState(0);
 
   useEffect(() => {
     let q = null;
@@ -51,19 +57,44 @@ export default function Dashboard() {
 
     const unsubscribe = onSnapshot(q, (snapshot) => {
       const challanData = [];
-
+      let totalAmountLocal = 0;
+      let totalAmountCollectedLocal = 0;
+      let successfulPaymentLocal = 0;
+      let successfulDeliveryLocal = 0;
       snapshot.forEach((doc) => {
         challanData.push({ id: doc.id, ...doc.data() });
+        
+        totalAmountLocal += doc.data().total_amount;
+
+        if (doc.data().payment_status == "Done") {
+          successfulPaymentLocal++;
+          totalAmountCollectedLocal += doc.data().total_amount;
+        }
+        
+        if (doc.data().delivery_status == "Done") successfulDeliveryLocal++;
       });
 
+      setTotalChallan(challanData.length);
+      setTotalAmount(totalAmountLocal);
+      setTotalAmountCollected(totalAmountCollectedLocal);
+      setSuccessfulDelivery(successfulDeliveryLocal);
+      setSuccessfulPayments(successfulPaymentLocal);
       
       setChallanList(challanData);
+
       setIsLoading(false);
     });
     return () => unsubscribe();
   }, [startDate, endDate]);
   return (
     <div>
+      <AnalyticsCard
+        totalChallan={totalChallan}
+        totalAmount={totalAmount}
+        totalAmountCollected={totalAmountCollected}
+        successfulPayments={successfulPayments}
+        successfulDelivery={successfulDelivery}
+      />
       <ChallanTable challanList={challanList} isLoading={isLoading} />
     </div>
   );
